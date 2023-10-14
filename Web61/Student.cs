870e06234c7ac9b46f4445b128c0e1009ac9583e
trungnguyendev <trungvjppro23@gmail.com>
2023-10-15 03:13:24 +0700

    test action

---
+++ Web61/Student.cs
@@ -0,0 +1,10 @@
+﻿namespace Web61
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+
+    }
+}
